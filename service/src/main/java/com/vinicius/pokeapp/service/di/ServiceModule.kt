e59c1c8c7568114727b9691601ea657e7c628064
Vinicius Silva <viniciusjns@gmail.com>
2021-10-07 19:08:34 -0300

    refactoring service configuration

--- service/src/main/java/com/vinicius/pokeapp/service/di/ServiceModule.kt
+++ service/src/main/java/com/vinicius/pokeapp/service/di/ServiceModule.kt
@@ -1,28 +1,67 @@
 package com.vinicius.pokeapp.service.di
 
-import com.vinicius.pokeapp.service.service.APIClient
-import com.vinicius.pokeapp.service.service.APIClientImpl
+import android.app.Application
+import com.squareup.moshi.Moshi
 import com.vinicius.pokeapp.service.service.PokeappService
 import dagger.Module
 import dagger.Provides
+import dagger.Reusable
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import javax.inject.Singleton
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 @Module
-open class ServiceModule {
+object ServiceModule {
 
     @Provides
-    @Singleton
-    fun providesApiClient(): APIClient = APIClientImpl()
+    @Reusable
+    @JvmStatic
+    internal fun provideCache(application: Application): Cache {
+        val cacheSize = (1024 * 1024 * 10).toLong() // 10MB
+        return Cache(application.cacheDir, cacheSize)
+    }
 
-    @Singleton
     @Provides
-    fun providesService(retrofit: Retrofit): PokeappService =
-        retrofit.create(PokeappService::class.java)
+    @Reusable
+    @JvmStatic
+    internal fun provideOkHttpClient(cache: Cache): OkHttpClient {
+        return OkHttpClient.Builder()
+            .cache(cache)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .build()
+    }
 
     @Provides
-    @Singleton
-    fun provideRetrofit(apiClient: APIClient): Retrofit =
-        apiClient.configure("https://pokeapi.co/api/v2/")
+    @Reusable
+    @JvmStatic
+    internal fun provideMoshiClient(): MoshiConverterFactory {
+        val moshi = Moshi.Builder()
+            .build()
+
+        return MoshiConverterFactory.create(moshi)
+    }
+
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun provideRetrofitInterface(okHttpClient: OkHttpClient,
+                                          moshiConverterFactory: MoshiConverterFactory,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://pokemon-db-json.herokuapp.com/")
+            .addConverterFactory(moshiConverterFactory)
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun provideMarvelApi(retrofit: Retrofit): PokeappService {
+        return retrofit.create(PokeappService::class.java)
+    }
 
 }
--- service/src/main/java/com/vinicius/pokeapp/service/service/APIClient.kt
+++
@@ -1,7 +0,0 @@
-package com.vinicius.pokeapp.service.service
-
-import retrofit2.Retrofit
-
-interface APIClient {
-    fun configure(baseUrl: String): Retrofit
-}
